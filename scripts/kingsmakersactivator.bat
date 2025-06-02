2d5da017473a591d651c6e1683e6478ed7172d70
Omar Osman Mahat <74245520+thekingsmakers@users.noreply.github.com>
2025-06-02 20:53:36 +0300

    Delete scripts directory

--- scripts/kingsmakersactivator.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-echo Running Thekingsmakers Activator Remotely ......
-
-powershell -Command "irm https://get.activated.win | iex"
-
-echo complete the activation in the massgrave prompt ... 
-
-pause
